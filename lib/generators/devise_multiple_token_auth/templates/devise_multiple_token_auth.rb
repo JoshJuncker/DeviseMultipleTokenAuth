fbe57886714a45a2c3025c9c5db63e9247bedf3c
Josh Juncker <josh@ringseven.com>
2015-08-28 22:43:35 -0600

    Added comments to config file

--- lib/generators/devise_multiple_token_auth/templates/devise_multiple_token_auth.rb
+++ lib/generators/devise_multiple_token_auth/templates/devise_multiple_token_auth.rb
@@ -1,1 +1,3 @@
+# set how many days of inactivity until a token expires.  
+# Note - each time a user successfully authenticates, the token expired date will be renewed by the same amount of days
 DeviseMultipleTokenAuth.device_expires_in_days = 14
