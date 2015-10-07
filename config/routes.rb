704bc0219fcbd650dfdcf47935e1e43bf72bd521
Josh Juncker <josh@ringseven.com>
2015-10-07 12:09:21 -0600

    Updated it all to fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   namespace :devise_multiple_token_auth do
     post "auth/login", to: "token_authentication#login", defaults: { format: 'json'}
     delete "auth/logout", to: "token_authentication#logout", defaults: { format: 'json'}
-    # put "update_push_token", to: "device_update_controller#update_push_token", defaults: { format: 'json'}
+    put "update_push_token", to: "device_update_controller#update_push_token", defaults: { format: 'json'}
   end
 end
--- lib/generators/devise_multiple_token_auth/templates/migrations/create_devise_multiple_token_auth_devices.rb
+++ lib/generators/devise_multiple_token_auth/templates/migrations/create_devise_multiple_token_auth_devices.rb
@@ -2,7 +2,7 @@ class CreateDeviseMultipleTokenAuthDevices < ActiveRecord::Migration
   def change
     create_table :devise_multiple_token_auth_devices do |t|
       t.belongs_to :user, index: true
-      t.string :auth_token
+      t.string :auth_token, null:false, index: {unique: true}
       t.datetime :expires_at
       t.timestamps null: false
     end
