49fab97423d395df846ceaba5a83d9865923d606
Josh Juncker <josh@ringseven.com>
2015-10-07 15:33:06 -0600

    Added scope for with_push_tokens

--- app/models/devise_multiple_token_auth/device.rb
+++ app/models/devise_multiple_token_auth/device.rb
@@ -2,6 +2,7 @@ class DeviseMultipleTokenAuth::Device < ActiveRecord::Base
   belongs_to :user
 
   scope :expired, -> {where("expires_at < ?",Time.now)}
+  scope :with_push_tokens, -> {where("push_token IS NOT NULL")}
 
   def self.destroy_expired
     DeviseMultipleTokenAuth::Device.expired.destroy_all
--- lib/devise_multiple_token_auth/version.rb
+++ lib/devise_multiple_token_auth/version.rb
@@ -1,3 +1,3 @@
 module DeviseMultipleTokenAuth
-  VERSION = "0.0.18"
+  VERSION = "0.0.19"
 end
